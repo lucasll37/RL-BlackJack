2965042166d260c5444e3a2a7ac07d232896d326
Lucas Lima <lucas.silva1037@gmail.com>
2024-07-15 11:04:18 -0300

    up

--- src/base/agent.py
+++ src/base/agent.py
@@ -24,10 +24,47 @@ class BJAgent(ABC):
         self.n_actions = self.env.action_space.n
         self.n_states = 0
         self.delta = 0
-        self.last_Q = None
         self.history = []
         self.validate_each_episodes = None
+        self.dimensions_states = None
+        self.last_Q = None
+        self.Q = None
+        self.N = None
+        self._init_data_structures()
 
+        # _dimensions = defaultdict(float)
+        # _observation_space = self.env.observation_space
+        # self.dimensions_states = len(_observation_space)
+
+        # for i in range(self.dimensions_states):
+        #     _dimensions[i] = list(range(_observation_space[i].n))
+
+        # indexes = [0] * self.dimensions_states
+
+        # while True:
+        #     key = list()
+
+        #     for i in range(self.dimensions_states):
+        #         key.append(_dimensions[i][indexes[i]])
+
+        #     self.map_state_Q[tuple(key)] = self.n_states
+        #     self.n_states += 1
+
+        #     indexes[0] += 1
+
+        #     for i in range(self.dimensions_states - 1):
+        #         if indexes[i] == len(_dimensions[i]):
+        #             indexes[i] = 0
+        #             indexes[i + 1] += 1
+
+        #     if indexes[-1] == len(_dimensions[len(_dimensions) - 1]):
+        #         break
+
+        # self.Q = np.random.uniform(low=-1, high=1, size=(self.n_states, self.n_actions))
+        # self.N = np.zeros(self.n_states)
+    
+
+    def _init_data_structures(self):
         _dimensions = defaultdict(float)
         _observation_space = self.env.observation_space
         self.dimensions_states = len(_observation_space)
@@ -61,7 +98,6 @@ class BJAgent(ABC):
 
 
     def learn(self, episodes=10_000, final_epsilon=0.01, epsilon_decay=None, epsilon_val=None, validate_each_episodes=None, verbose=True, save=True):
-
         raise NotImplementedError('Please implement this method')
 
 
@@ -101,16 +137,8 @@ class BJAgent(ABC):
         env.close()
 
         return win, lose
-
-    def close(self):
-        self.env.close()
-
-    def reset_learning(self):
-        self.Q = np.random.uniform(low=-1, high=1, size=(self.n_states, self.n_actions))
-        self.N = np.zeros(self.n_states)
-        self.epsilon = self.initial_epsilon
-        self.history = []
-
+    
+        
     def plot_history(self, return_fig=False):
         if len(self.history) == 0:
             raise ValueError("No history to plot")
@@ -149,57 +177,6 @@ class BJAgent(ABC):
             return fig
         
         
-    def fit_exponential(self, x, y):
-
-        def curve(x, a, b, c):
-            return a * (1 - np.exp(-b * x)) + c
-        
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", category=RuntimeWarning)
-
-            params, _ = curve_fit(curve, x, y)
-            a, b, c = params        
-            y_fitted = curve(x, a, b, c)
-            asymptote = a + c
-            
-        return y_fitted, asymptote
-    
-
-    def validation(self, episode, episodes, epsilon_val, verbose, save):
-
-        if not isinstance(epsilon_val, (int, float)):
-            _epsilon_val = self.epsilon
-        else:
-            _epsilon_val = epsilon_val
-            
-        result = self.play(episodes=1_000, render_mode=None, print_results=False, epsilon=_epsilon_val)
-        self.history.append(result)
-            
-        if verbose:
-            print(f"Episode: {episode:7d}/{episodes}, epsilon: {self.epsilon:.5f}, delta: {self.delta:.2e}, Win rate: {result[0]/(result[0]+result[1]):.3f}")
-
-        if save:
-            save_agent(self, f"./models/{self.name}.pickle")
-
-
-    def epsilon_update(self, episodes, validate_each_episodes, final_epsilon, epsilon_decay):
-        self.validate_each_episodes = validate_each_episodes
-
-        if isinstance(epsilon_decay, (int, float)):
-            epsilon_decay_factor = epsilon_decay
-        else:
-            epsilon_decay_factor = np.power(final_epsilon, 1/episodes)
-
-        return epsilon_decay_factor
-    
-
-    def _plot_points(self, ax, point_list, style, label, alpha=0.8):
-
-        if point_list:
-            x, y = zip(*[(point[0], point[1]) for point in point_list])
-            ax.scatter(x, y, s=150, c=style, label=label, alpha=alpha)
-
-
     def plot_policy(self, return_fig=False):
 
         player_count = range(3, 22)
@@ -232,42 +209,101 @@ class BJAgent(ABC):
                         elif action == 1:
                             push_hit_ace.append(state)
 
-        plt.rcParams["savefig.dpi"] = 300
-        fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+        fig, ax = plt.subplots(figsize=(14, 6))
+        fig.suptitle("[MonteCarlo] BLACKJACK - POLICY", fontsize=16)
 
-        fig.suptitle(f"[{self.name}] BLACKJACK - POLICY", fontsize=16)
+        for pc in player_count:
+            for dc in dealer_count:
+                state_no_ace = (pc, dc, 0)
+                state_ace = (pc, dc, 1)
 
-        # Plotting no ace
-        ax1 = axes[0]
-        self._plot_points(ax1, push_stand_no_ace, 'gray', 'Stand', alpha=0.3)
-        self._plot_points(ax1, push_hit_no_ace, 'b', 'Hit')
-        ax1.set_xlabel("Player Count", color="blue", fontsize=14)
-        ax1.set_ylabel("Dealer Count", color="blue", fontsize=14)
-        ax1.set_title("Agent Policy (No Ace)", fontsize=14)
-        ax1.set_xticks(player_count)
-        ax1.set_yticks(dealer_count)
-        ax1.tick_params(axis="x", colors="blue")
-        ax1.tick_params(axis="y", colors="blue")
+                if state_no_ace in push_hit_no_ace and state_ace in push_hit_ace:
+                    color = 'blue'  # HIT in both cases
+                elif state_no_ace not in push_hit_no_ace and state_ace in push_hit_ace:
+                    color = 'orange'  # HIT only with ace
+                elif state_no_ace in push_hit_no_ace and state_ace not in push_hit_ace:
+                    color = 'yellow'  # HIT only without ace
+                else:
+                    color = 'gray'  # STAND in any case
 
-        # Plotting with ace
-        ax2 = axes[1]
-        self._plot_points(ax2, push_stand_ace, 'gray', 'Stand', alpha=0.3)
-        self._plot_points(ax2, push_hit_ace, 'b', 'Hit')
-        ax2.set_xlabel("Player Count", color="blue", fontsize=14)
-        ax2.set_ylabel("Dealer Count", color="blue", fontsize=14)
-        ax2.set_title("Agent Policy (With Ace)", fontsize=14)
-        ax2.set_xticks(player_count)
-        ax2.set_yticks(dealer_count)
-        ax2.tick_params(axis="x", colors="blue")
-        ax2.tick_params(axis="y", colors="blue")
+                ax.plot(pc, dc, 'o', color=color, markersize=10)
 
-        # Ajuste a legenda para fora da área da figura
-        handles, labels = ax1.get_legend_handles_labels()
-        fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=2)
+        ax.set_xlabel("Player Count", color="blue", fontsize=14)
+        ax.set_ylabel("Dealer Count", color="blue", fontsize=14)
+        ax.set_xticks(player_count)
+        ax.set_yticks(dealer_count)
+        ax.tick_params(axis="x", colors="blue")
+        ax.tick_params(axis="y", colors="blue")
 
-        plt.tight_layout(rect=[0, 0, 1, 0.95])
+        legend_elements = [
+            plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='blue', markersize=10, label='Hit in both cases'),
+            plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='orange', markersize=10, label='Hit only with ace'),
+            plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='yellow', markersize=10, label='Hit only without ace'),
+            plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='gray', markersize=10, label='Stand in any case')
+        ]
+
+        ax.legend(handles=legend_elements, loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=2)
+        # plt.tight_layout(rect=[0, 0, 1, 0.95])
         plt.show()
         plt.close()
 
         if return_fig:
             return fig
+        
+
+    def fit_exponential(self, x, y):
+
+        def curve(x, a, b, c):
+            return a * (1 - np.exp(-b * x)) + c
+        
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+
+            params, _ = curve_fit(curve, x, y)
+            a, b, c = params        
+            y_fitted = curve(x, a, b, c)
+            asymptote = a + c
+            
+        return y_fitted, asymptote
+    
+
+    def epsilon_update(self, episodes, validate_each_episodes, final_epsilon, epsilon_decay):
+        self.validate_each_episodes = validate_each_episodes
+
+        if isinstance(epsilon_decay, (int, float)):
+            epsilon_decay_factor = epsilon_decay
+        else:
+            epsilon_decay_factor = np.power(final_epsilon, 1/episodes)
+
+        return epsilon_decay_factor
+    
+
+    def validation(self, episode, episodes, epsilon_val, verbose, save):
+
+        if not isinstance(epsilon_val, (int, float)):
+            _epsilon_val = self.epsilon
+        else:
+            _epsilon_val = epsilon_val
+            
+        result = self.play(episodes=1_000, render_mode=None, print_results=False, epsilon=_epsilon_val)
+        self.history.append(result)
+            
+        if verbose:
+            print(f"Episode: {episode:7d}/{episodes}, epsilon: {self.epsilon:.5f}, delta: {self.delta:.2e}, Win rate: {result[0]/(result[0]+result[1]):.3f}")
+
+        if save:
+            save_agent(self, f"./models/{self.name}.pickle")
+
+
+    def reset_learning(self):
+        self.Q = np.random.uniform(low=-1, high=1, size=(self.n_states, self.n_actions))
+        self.N = np.zeros(self.n_states)
+        self.epsilon = self.initial_epsilon
+        self.history = []
+        self.validate_each_episodes = None
+        self.last_Q = None
+        self.delta = 0
+
+
+    def close(self):
+        self.env.close()
